d1f61f058e5a65d6d2e7b35dd416fc1d3f06354f
OCTAVIO <octavioticona.a20@outlook.com>
2026-02-18 22:31:14 -0400

    refactor: Correción para eslint y cypress,

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -15,7 +15,15 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        test: "readonly",
+        expect: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
